e8f93815699875f637d7decda0ea56fb8ff1b79b
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-12-07 14:52:14 +0100

    commit

--- src/products/index.js
+++ src/products/index.js
@@ -1,17 +1,53 @@
-import express from "express"
-import { ProductModel } from "./model.js"
+import express from "express";
+import { ProductModel } from "./model.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter
-    .get('/', async (req, res) => {
-        const products = await ProductModel.find({})
-        res.send(products)
-    })
-    .post("/", async (req, res) => {
-        const product = new ProductModel(req.body)
-        await product.save()
-        res.status(201).send(product)
-    })
+  .get("/", async (req, res) => {
+    const products = await ProductModel.find({});
+    res.send(products);
+  })
+  .post("/", async (req, res) => {
+    const product = new ProductModel(req.body);
+    await product.save();
+    res.status(201).send(product);
+  });
 
-export default productsRouter
+productsRouter
+  .get("/:id", async (req, res) => {
+    try {
+      const product = await ProductModel.findById(req.params.id);
+      res.status(200).send({ product });
+    } catch (error) {
+      next(error);
+    }
+  })
+  .put("/:id", async (req, res) => {
+    try {
+      const editedProduct = await ProductModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.send({ editedProduct });
+    } catch (error) {
+      next(error);
+    }
+  })
+  .delete("/:id", async (req, res) => {
+    try {
+      const reviews = await reviewsModel.deleteMany({
+        productId: req.params.id,
+      });
+      const deletedProduct = await ProductModel.findByIdAndDelete(
+        req.params.id
+      );
+      // const deletedReviews = reviews.deleteMany({})
+      res.status(204).send({ message: `${req.params.id} deleted!!` });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+export default productsRouter;
